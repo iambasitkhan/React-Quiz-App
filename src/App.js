60b7fa255178908bfb7d4b118dbbce89c3e94bed
Basitkhan <abasitkk92@gmail.com>
2023-09-10 20:45:06 +0500

    Working on DataRecieved and Error Handling is Done..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.paylaod, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      return "unknown Action";
+  }
+};
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
 
 export default function App() {
-  return <div>{/* <DateCounter /> */}</div>;
+  const [{ questions }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const resp = await fetch(`http://localhost:8000/questions`);
+        const data = await resp.json();
+        dispatch({ type: "dataRecieved", paylaod: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
+  console.log(questions);
+  return (
+    <div className="app">
+      <Header />
+      <MainContainer></MainContainer>
+    </div>
+  );
 }
