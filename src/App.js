ebed26fd9eadd0c4ed8c47e5d3cafaf536dd67f8
Basitkhan <abasitkk92@gmail.com>
2023-09-10 21:38:46 +0500

    Strat screen for the Quiz deesigned

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
 
 const reducer = function (state, action) {
   switch (action.type) {
@@ -19,7 +22,10 @@ const initialState = {
 };
 
 export default function App() {
-  const [{ questions }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  // Derived State
+  const numQuestions = questions.length;
 
   useEffect(function () {
     async function fetchQuestions() {
@@ -35,11 +41,14 @@ export default function App() {
     fetchQuestions();
   }, []);
 
-  console.log(questions);
   return (
     <div className="app">
       <Header />
-      <MainContainer></MainContainer>
+      <MainContainer>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </MainContainer>
     </div>
   );
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,9 @@
+export default function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quize!</h2>
+      <h3>{numQuestions} of Questions to test your React mastery.</h3>
+      <button className="btn btn-ui">Let`s Start</button>
+    </div>
+  );
+}
